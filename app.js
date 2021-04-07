793debf64f2109df3d477a8b19faabab52658c99
00009974 <jt00009974@gmail.com>
2021-04-07 23:31:21 +0500

    added rest api

--- app.js
+++ app.js
@@ -18,83 +18,92 @@ app.use(express.urlencoded({ extended: false}));
 app.get('/', (req, res) => {
     fs.readFile('./data/notes.json', (err, data) => {
         if (err) throw err
-
-        const articles = JSON.parse(data)
-
-        res.render('index', {articles: articles});
+        
+        const posts = JSON.parse(data)
+        
+        res.render('index', {articles: posts});
     })
 })
 
 
 
 app.get('/articles', (req, res) => {
-
+    
     fs.readFile('./data/notes.json', (err, data) => {
         if (err) throw err
-
+        
         const articles = JSON.parse(data)
-
+        
         res.render('articles', {articles: articles});
     })
 })
 
 app.get('/create', (req, res) => {
     res.render('create', { error: null })
-  });
+});
 
 app.post('/create', (req, res) => {
     const title = req.body.title
     const blog = req.body.blog
     const author = req.body.author
-
+    
     if (title.trim() === '' && blog.trim() === '' && author.trim() === '') {
         res.render('create', { error: true });
     } else {
         fs.readFile('./data/notes.json', (err, data) => {
             if (err) throw err
-
+            
             const notes = JSON.parse(data)
-
+            
             notes.push({
                 id: id (),
                 title: title,
                 blog: blog,
                 author: author, 
             })
-
+            
             fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
                 if (err) throw err
-
-                res.render('index')
+                
+                res.render('index', {articles: notes})
             })
         })
     }
-
-    res.render("index");
 })
+
+app.get('/api/v1/articles', (req, res) => {
+    fs.readFile('./data/notes.json', (err, data) => {
+        if(err) throw err
+        
+        const articles = JSON.parse(data)
+        
+        res.json(articles)
+    })
+    
+});
 
 app.get('/:id', (req, res) => {
     const id = req.params.id
-
+    
     fs.readFile('./data/notes.json', (err, data) => {
         if (err) throw err
-
+        
         const articles = JSON.parse(data)
-
+        
         const article = articles.filter(article => article.id === id)[0]
         
         res.render("detail", { article: article });
     })
-
+    
 })
 
 // server
 app.listen(8000, err => {
     if(err) console.log(err)
-
+    
     console.log('Server is running on port 8000...')
 });
 
 function id () {
     return '_' + Math.random().toString(36).substr(2, 9);
-  };
+};
